3e2c7d8a03c83a9f45253990ec1aef0da2fcb1a8
Erik <rainer.erik70@gmail.com>
2024-05-07 09:58:44 -0700

    folder structure fixed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const generateMarkdown = require("./generateMarkdown");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
